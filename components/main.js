74b2d88f73d7c131762a6e36af0ab28eea4f4736
unknown <spencer.woo@liferay.com>
2019-01-10 20:34:16 -0800

    main (image) component and style

--- components/main.js
+++ components/main.js
@@ -1,18 +1,45 @@
-class Main extends React.Component {
+//Main(image, title, title2, description)
 
+// Used for :
+// Welcome, CYF, Vision, CTs, CLAY, Young Family Fellowship, Friends Of Jesus
+// English Adult Fellowship, Zion, Mandarin Fellowship, Statement of Faith
+// Upcoming Events, Mission Trips, Tijuana, Pala, Many Farms, Honduras
+// Belize, East Asia, Fellowshop Groups, Event Name, Blast, etc
+
+class Main extends React.Component {
+  //Probably should take a MainObject instead of individual props.
   render() {
-    console.log(this.props);
     return (
-      <div>
-      	<img src="../img/church.png" id="intro-image"/>
-      	<h2>{this.props.title}</h2>
-      	<h3>{this.props.description}</h3>
+      <div className="intro">
+        <img src={"../img/" + this.props.image} id="intro-image"/>
+        
+        <h2>{this.props.title}</h2>
+        <h2>{this.props.title2}</h2>
+        <h3>{this.props.description}</h3>
+
+        <div className="buffer"></div>
       </div>
     );
   }
 }
 
 ReactDOM.render(
-  <Main/>,
+  <Main 
+    image="church.png" 
+    title="WELCOME" 
+    title2="" 
+    description="Come for a visit this Sunday!"
+  />,
   document.getElementById('intro-container')
 );
+
+
+
+//  CYF
+        // <h2>CHRISTIAN YOUTH</h2>
+       //  <h2>FELLOWSHIP</h2>
+        // <h3>Junior High & High School</h3>
+
+// WELCOME
+        // <h2>WELCOME</h2>
+        // <h3>Come for a visit this Sunday!</h3>
