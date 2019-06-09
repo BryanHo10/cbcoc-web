b1e759ef0b745b9d4aad75f15c73453597bdd787
BryanHo10 <bryankho1010@gmail.com>
2019-06-09 00:04:54 -0400

    update comments on baptist.js

--- src/components/statement/baptist.js
+++ src/components/statement/baptist.js
@@ -1,12 +1,21 @@
 import React from "react"
 import statementFaith from "../../data/baptist_faith_message"
 
+// Dispays in webpage the Baptist Faith and Message (taken from their website)
+// Constructed a json file that has the parsed information (topic/info)
+
+// JSON-Mapping:    key:(string)            Topic (i.e. The Scriptures,God,God the Father,...)
+//                  value:(List<string)     Info
+
 class BaptistFaithMessage extends React.Component{
     render(){
         return(
             <div className="container-fluid row">
+
+                {/* Handles the content */}
                 <div id="statement-main" className="col-md-9">
                 {
+                    // Iterates through Topics for styling and attaching the information underneath 
                     Object.keys(statementFaith).map((topic)=>{
                         let linkID=topic.replace(/ /g,'_');
                         return(
@@ -25,8 +34,11 @@ class BaptistFaithMessage extends React.Component{
                     })
                 }
                 </div>
+
+                {/* Handles Sidebar Navigation */}
                 <div id="sidenav" className="col-md-2">
                     {
+                        // Lists all topics
                         Object.keys(statementFaith).map((topic)=>{
                             let linkID=topic.replace(/ /g,'_');
                             return(
