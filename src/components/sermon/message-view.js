55434fd3b8ada311eb5f4c642e77c39edc99b554
BryanHo10 <bryankho1010@gmail.com>
2019-06-08 23:52:15 -0400

    update comments on sermon components

--- src/components/sermon/message-view.js
+++ src/components/sermon/message-view.js
@@ -1,24 +1,34 @@
 import React from "react"
 
-/* PROPS : 
-        "id": 156,
-        "date": "2018-11-18",
-        "title": "A True Servant and a False Kin",
-        "series": "Mark",
-        "series_num": 17,
-        "passage": "Mark 6:14-29",
-        "speaker": "Pastor Ryan Cheung",
-        "audio_link": "http://www.cbcoc.org/content/media/audio/englishworship/english_2018_11_18_cheung.mp3"
+// Displays Sermon information (Date,Passage,Speaker,Title,Series)
+// Includes an in-Browser audio player that links to the mp3 file
+// Buttons with options to download or direct to hyperlink
+
+// Todo: Set up Database Management / Tool to add sermons / Tool to export sermons from DB to JSON
+
+/* Props: 	id: (int)				index position (more recent = higher index)
+			date: (string)			"Year-Month-Day"
+			title: (string)			title of sermon
+			series: (string)		category of sermon
+			series_num: (int)		index position in category
+			passage: (string)		Scripture reference (Book Chapter:Verse(s))
+			speaker: (string)		name of speaker (e.g. "Pastor Ryan Cheung")
+			audio_link: (string)	hyperlink to audio file
+
 */
+
 
 class MessageView extends React.Component {
 	constructor(props){
 		super(props);
+		
+		// Might want to use this.state to hold variables 
 		this.state = {
 			
 		};
 	}
 	render(){
+		// Strips date of necessary information (Day,Month,Year)
 		let dateString = new Date(this.props.date).toDateString();
 		let dateValues = dateString.split(' '); // [Wed, Jul, 28, 1993]
 		let month = dateValues[1];
@@ -28,25 +38,38 @@ class MessageView extends React.Component {
 
 		return(
 			<div className="container-fluid row py-3">
+
+				{/* Lef Column */}
 				<div className="col-lg-2 text-center">
+					{/* Dispaly Date */}
 					<h5 className="gotham-bold">{year}</h5>	
 					<h1 className="gotham-bold">{month}<br/>{day}</h1>	
 				</div> 
+
+				{/* Center Column */}
 				<div className="col-lg">
+					{/* Sermon Information */}
 					<h2 id="message-title" className="gotham-med">{this.props.title}</h2>
 					<h5 className="gotham-book pb-4">{this.props.speaker}<br/>{this.props.passage}</h5>
 
+					{/* Browser Audio Player (check if compatible with all browsers) */}
 					<audio controls className="container-fluid">
 						<source src={this.props.audio_link} />
 					</audio>	
 				</div>
+
+				{/* Right Column */}
 				<div className="col-lg-2">
+
+					{/* Download Button */}
 					<div>
 						<a download="message" href={this.props.audio_link}>
 							<h6 className="fa-solid" id="attach-icon"></h6>
 							<h6 id="attach-icon" className="gotham-book">  Download</h6>
 						</a>
 					</div>
+
+					{/* Link-to Button */}
 					<div>
 						<a href={this.props.audio_link}>
 							<h6 className="fa-solid" id="attach-icon"></h6>
--- src/components/sermon/sermon-nav.js
+++ src/components/sermon/sermon-nav.js
@@ -1,13 +1,25 @@
 import React from "react"
 import {Link} from "gatsby"
 
+// Navigates to newer and older sermons
+
+// Todo: Implement Pagination
+/* Props:       hideNext: (bool)        Can we go to the next page?
+                hideBack: (bool)        Can we go back one more page?
+                currentPage: (int)      page number
+*/
 class SermonNav extends React.Component {
     render(){
+
+        // Handles visibility of Navigation buttons
         let endNewZone={display:`${this.props.hideNext ? "none":"block"}`};
         let endOldZone={display:`${this.props.hideBack ? "none":"block"}`};
+
         return(
             <div className="container-fluid">
                 <div className="d-flex">
+
+                    {/* Handles Previous Page Button */}
                     <div className="mr-auto px-5 py-4">
                         <Link   style={endOldZone}
                                 id="backtrack-events" 
@@ -20,6 +32,8 @@ class SermonNav extends React.Component {
                                     <h6 id="attach-icon"> Back  </h6>
                         </Link>
                     </div>
+                    
+                    {/* Handles Next Page Button */}
                     <div className="px-5 py-4">
                         <Link   style={endNewZone}
                                 id="backtrack-events" 
